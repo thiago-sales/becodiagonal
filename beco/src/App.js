a7e73b640880afea47e57ddcc049b3c5777486db
thiago-sales <56303073+thiago-sales@users.noreply.github.com>
2022-11-17 15:18:41 +0000

    links

--- beco/src/App.js
+++ beco/src/App.js
@@ -6,6 +6,16 @@ function App() {
       
     <h1>Title</h1>
 
+    <h2>Inglês: </h2>
+    <br/>
+      <ul>
+        <a href='https://drive.google.com/file/d/1-3vn4Lg6fdGxSPy4uzqeq_19toan_5ut/view?usp=share_link'>
+          <li>Atividade 1</li></a>
+        <a href='https://drive.google.com/file/d/1xbcCQlWg255dDa6y1Qb87-90bEn6luK1/view?usp=sharing'>
+          <li>Atividade 2</li></a>
+      </ul>
+
+
     </div>
   );
 }
